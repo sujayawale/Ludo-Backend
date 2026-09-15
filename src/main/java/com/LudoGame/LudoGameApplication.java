babb0c012443c61a651ec6587a5a3013841c3dc0
Sujay Awale <sujayawale125@gmail.com>
2026-09-16 00:24:59 +0530

    added working project

---
+++ src/main/java/com/LudoGame/LudoGameApplication.java
@@ -0,0 +1,13 @@
+package com.LudoGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudoGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LudoGame/LudoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LudoGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
